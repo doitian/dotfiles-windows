42b34271b7a663d0b9c1a388bacc21dadd20eff3
ian <me@iany.me>
2020-06-13 10:24:16 +0800

    add ps

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-Set-PSReadLineOption -EditMode emacs -Colors @{
+﻿Set-PSReadLineOption -EditMode emacs -Colors @{
   "Member" = "$([char]0x1b)[37m"
   "Number" = "$([char]0x1b)[37m"
 }
@@ -12,6 +12,7 @@ $env:EDITOR = 'vim'
 $env:FZF_DEFAULT_OPTS = '--color=light --color=fg:0'
 
 Set-Alias -Name g -Value git
+Set-Alias -Name j -Value z
 Set-Alias -Name grep -Value rg
 Set-Alias -Name which -Value Get-Command
 
@@ -19,11 +20,32 @@ $cb = "$HOME/codebase"
 $dcs = "$HOME/Documents"
 $dsk = [Environment]::GetFolderPath("Desktop")
 $dl = "$HOME/Downloads"
-$kb = "$HOME/codebase/knowledge-base"
+$kb = "$HOME/codebase/my/knowledge-base"
 
 function fpass {
   pushd "~/.password-store"
   $selected = (fzf) -Replace '\.gpg$', ''
   popd
   gopass @Args $selected
+}
+
+function prompt {
+  $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+  $principal = [Security.Principal.WindowsPrincipal] $identity
+  $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+  $cwd = (Get-Location).Path
+  $parts = $cwd.Split([IO.Path]::DirectorySeparatorChar)
+  if ($parts.Count -gt 4) {
+    $cwd = $parts[0..1] + @("…") + $parts[-2..-1] -join '\'
+  }
+  Write-Host $cwd -NoNewline -ForegroundColor Blue
+
+  $(if (Test-Path variable:/PSDebugContext) {
+    "? "
+  } elseif ($principal.IsInRole($adminRole)) {
+    "# "
+  } else {
+    "$ "
+  })
 }
