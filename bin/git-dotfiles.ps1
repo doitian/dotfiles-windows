6784e49f0b85fe379178101c8ed9d82043fbc4ae
ian <me@iany.me>
2020-06-15 17:41:23 +0800

    update git-dotfiles.ps1

--- bin/git-dotfiles.ps1
+++ bin/git-dotfiles.ps1
@@ -1,1 +1,8 @@
-git-multistatus ~/.dotfiles/repos/public ~/.dotfiles/repos/private ~/codebase/my/dotfiles-windows.git
+$ReposDir = Get-Item "$HOME\.dotfiles\repos"
+if ($ReposDir.Target -ne $null) {
+  $ReposDir = $ReposDir.Target
+}
+$PublicRepoDir = "$ReposDir\public"
+$PrivateRepoDir = "$ReposDir\private"
+
+git-multistatus $PublicRepoDir $PrivateRepoDir (Split-Path $PROFILE)
