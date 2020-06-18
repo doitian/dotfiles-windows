58329b8ef91ba9851306df744d724ac65b20f0ff
ian <me@iany.me>
2020-06-18 21:44:03 +0800

    Convert to bomb to support unicode in old PS

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+﻿[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 Set-PSReadLineOption -EditMode emacs -Colors @{
   "Command" = "Magenta"
