067e072fa7f1e542f024ed68f5812dc9e4720e12
ian <me@iany.me>
2020-10-20 14:54:35 +0800

    upgrade clash to 1.2.0

--- bin/gfw.ps1
+++ bin/gfw.ps1
@@ -1,10 +1,10 @@
 if ($args.Count -gt 0) {
   $env:http_proxy = 'http://127.0.0.1:7890'
   $env:https_proxy = $env:http_proxy
-  $env:all_proxy = 'http://127.0.0.1:7891'
+  $env:all_proxy = 'http://127.0.0.1:7890'
   & $args[0] $args[1..($args.Count-1)]
 } else {
   $env:http_proxy = 'http://127.0.0.1:7890'
   $env:https_proxy = $env:http_proxy
-  $env:all_proxy = 'http://127.0.0.1:7891'
+  $env:all_proxy = 'http://127.0.0.1:7890'
 }
