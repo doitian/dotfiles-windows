43a99e745046dc3c35a2286dcad6146c9279152a
ian <me@iany.me>
2021-04-27 10:31:43 +0800

    Splat array in gfw

--- bin/gfw.ps1
+++ bin/gfw.ps1
@@ -2,7 +2,8 @@ if ($args.Count -gt 0) {
   $env:http_proxy = 'http://127.0.0.1:7890'
   $env:https_proxy = $env:http_proxy
   $env:all_proxy = 'http://127.0.0.1:7890'
-  & $args[0] $args[1..($args.Count-1)]
+  $RemainingArgs = $args[1..($args.Count-1)]
+  & $args[0] @RemainingArgs
 } else {
   $env:http_proxy = 'http://127.0.0.1:7890'
   $env:https_proxy = $env:http_proxy
