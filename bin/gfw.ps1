c4c26021226e2a958b8424d107940c8523de8caa
ian <me@iany.me>
2023-08-28 23:57:16 +0800

    gfw: unset after one-shot command

--- bin/gfw.ps1
+++ bin/gfw.ps1
@@ -8,6 +8,17 @@ $env:all_proxy = $env:http_proxy
 $env:no_proxy = 'localhost, 127.0.0.1, ::1'
 
 if ($args.Count -gt 0) {
-  $RemainingArgs = $args[1..($args.Count-1)]
-  & $args[0] @RemainingArgs
+  if ($args[0] -eq 'on') {
+    return
+  }
+
+  if ($args[0] -ne 'off') {
+    if ($args.Count -eq 1) {
+      & $args[0]
+    } else {
+      $RemainingArgs = $args[1..($args.Count-1)]
+      & $args[0] @RemainingArgs
+    }
+  }
+  rm env:http_proxy, env:https_proxy, env:all_proxy, env:no_proxy
 }
