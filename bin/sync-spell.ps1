f99958c2fef90bf594843c5cb8a247107bb0a1f5
ian <me@iany.me>
2024-04-14 21:02:21 +0800

    include obsidian in sync-spell.ps1

--- bin/sync-spell.ps1
+++ bin/sync-spell.ps1
@@ -1,7 +1,43 @@
-$files = @("$HOME\.vim-spell-en.utf-8.add", "$HOME\AppData\Roaming\Microsoft\Spelling\neutral\default.dic")
-$existingFiles = $files | ? { Test-Path $_ }
-$merged =  Get-Content $existingFiles | % { $_.Trim() } | ? { $_ -ne "" } | Sort-Object | Get-Unique
-foreach ($file in $files) {
-  echo "==> $file"
-  Set-Content -Path $file -Value $merged
+$files = @(
+  "$HOME\.vim-spell-en.utf-8.add"
+  "$HOME\Dropbox\Backups\General\spell.txt"
+)
+$winFile = "$HOME\AppData\Roaming\Microsoft\Spelling\neutral\default.dic"
+
+$existingFiles = ($files + $winFile) | ? { Test-Path $_ }
+$merged =  Get-Content $existingFiles | % { $_.Trim() } | ? { $_ -ne "" }
+
+$obsidianAppSettingPath = "$HOME\Dropbox\Brain\.obsidian\app.json"
+$obsidianAppSetting = $null
+
+if (Test-Path $obsidianAppSettingPath) {
+  $obsidianAppSetting = Get-Content $obsidianAppSettingPath | ConvertFrom-Json
+  $merged += $obsidianAppSetting.spellcheckDictionary
 }
+
+$merged = $merged | Sort-Object -Unique -CaseSensitive
+
+$outFiles = @()
+
+if (Test-Path (Split-Path -Parent $winFile)) {
+  $outFiles += Resolve-Path $winFile
+  Set-Content -Path $winFile -Value $merged
+}
+
+if ($obsidianAppSetting -ne $null) {
+  $obsidianAppSetting.spellcheckDictionary = $merged
+  $obsidianAppSettingJson = ($obsidianAppSetting | ConvertTo-Json -Depth 2) -Replace "`r`n", "`n"
+  $outFiles += Resolve-Path $obsidianAppSettingPath
+  Set-Content -NoNewline -Path $obsidianAppSettingPath -Value $obsidianAppSettingJson
+}
+
+# LF newline
+$merged = $merged | Join-String -Separator `n
+foreach ($file in $files) {
+  if (Test-Path (Split-Path -Parent $file)) {
+    $outFiles += Resolve-Path $file
+    Set-Content -NoNewline -Path $file -Value $merged
+  }
+}
+
+$outFiles
