91c12d969619c31b674ec7a4abce5572c21559d6
ian <me@iany.me>
2025-06-21 01:05:22 +0800

    simplify mise preset:rust

--- mise/tasks/preset/rust.ps1
+++ mise/tasks/preset/rust.ps1
@@ -3,7 +3,7 @@
 
 mise tasks add pre-commit:cargo-fmt -- cargo fmt --check
 if (Get-Command -ErrorAction Ignore cargo-nextest) {
-  mise tasks add test -- cargo nextest run --no-fail-fast --nocapture '{{vars.cargo_test_args}}'
+  mise tasks add test -- cargo nextest run --no-fail-fast --nocapture
 } else {
-  mise tasks add test -- cargo test --no-fail-fast -- --nocapture '{{vars.cargo_test_args}}'
+  mise tasks add test -- cargo test --no-fail-fast -- --nocapture
 }
