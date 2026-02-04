1fdb2830a6362769b0e6b4da18385da759021737
ian <me@iany.me>
2026-02-04 21:01:47 +0800

    remove unused Disable-AcronisServices function from toolkits.psm1, streamlining the script and improving maintainability

--- toolkits.psm1
+++ toolkits.psm1
@@ -1,16 +1,3 @@
-function Disable-AcronisServices() {
-  $services = Get-Service -DisplayName 'Acronis*'
-  $services | Stop-Service
-  $services | Set-Service -StartupType Disabled
-  echo "Disable Active Protect manually"
-  echo "Disable auto start programs in Task Manager"
-
-  $startDir = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Acronis\True Image\Tools and Utilities"
-  if (Test-Path -LiteralPath $startDir) {
-    rm -Re -Fo $startDir
-  }
-}
-
 function Get-FolderSize {
   [CmdletBinding()]
   Param (
