123d795d8d32161e5590d1347dee03e6a10396bd
ian <me@iany.me>
2026-03-03 22:01:32 +0800

    cleanup unused files

--- backup.ps1
+++ backup.ps1
@@ -12,7 +12,6 @@ $Dest = "\\10.31.0.5\Public\Backups"
 
 $Sources = @(
     @{ Path = "$HOME\Zotero";                    Name = "Zotero" }
-    @{ Path = "$HOME\Documents\Inventory";       Name = "Inventory" }
     @{ Path = "$HOME\AppData\Roaming\krita";     Name = "krita" }
 )
 
